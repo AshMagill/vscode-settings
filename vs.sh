cb9f7ffbdacd3b1f0030ca140610cec07b0d04d5
AshMagill <ashmagillnz@gmail.com>
2022-05-08 09:56:08 +1200

    first commit

---
+++ vs.sh
@@ -0,0 +1,8 @@
+code --install-extension tabnine.tabnine-vscode dsznajder.es7-react-js-snippets esbenp.prettier-vscode msjsdiag.debugger-for-chrome ms-vscode.js-debug-companion ms-vscode.references-view
+pkief.material-icon-theme sainnhe.gruvbox-material
+
+code --enable-proposed-api tabnine.tabnine-vscode dsznajder.es7-react-js-snippets esbenp.prettier-vscode msjsdiag.debugger-for-chrome ms-vscode.js-debug-companion ms-vscode.references-view
+pkief.material-icon-theme sainnhe.gruvbox-material
+
+
+
