5a18a571e3f7047925a1eb5c68e92ff6a1cee4c3
AshMagill <ashmagillnz@gmail.com>
2022-05-08 10:39:27 +1200

    added stuff

--- vs.sh
+++ vs.sh
@@ -1,8 +1,6 @@
-code --install-extension tabnine.tabnine-vscode dsznajder.es7-react-js-snippets esbenp.prettier-vscode msjsdiag.debugger-for-chrome ms-vscode.js-debug-companion ms-vscode.references-view
-pkief.material-icon-theme sainnhe.gruvbox-material
+#!/bin/bash
 
-code --enable-proposed-api tabnine.tabnine-vscode dsznajder.es7-react-js-snippets esbenp.prettier-vscode msjsdiag.debugger-for-chrome ms-vscode.js-debug-companion ms-vscode.references-view
-pkief.material-icon-theme sainnhe.gruvbox-material
+code --install-extension ./CachedExtensionVSIXs/*
 
 
 
